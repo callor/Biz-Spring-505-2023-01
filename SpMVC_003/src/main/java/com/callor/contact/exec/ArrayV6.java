979d916bf84759f91709b502486f87a9ff423273
callor <callor@callor.com>
2023-01-12 11:51:18 +0900

    array

--- SpMVC_003/src/main/java/com/callor/contact/exec/ArrayV6.java
+++ SpMVC_003/src/main/java/com/callor/contact/exec/ArrayV6.java
@@ -13,11 +13,22 @@ public class ArrayV6 {
 		// 분해된 문자열이 담긴 nations 배열의
 		// 각 알파벳을 무작위로 Shuffle 해 보자
 		for(int i = 0 ; i < nations.length ; i++) {
+			// 배열의 index 범위내에서 임의 정수 2개를 생성
 			int index1 = (int)(Math.random() * nations.length);
 			int index2 = (int)(Math.random() * nations.length);
+			
+			// nations[index1] , nations[index2] 두 요소의
+			// 값을 서로 교환
+			
+			// 임시 저장
+			String _n = nations[index1];
+			nations[index1] = nations[index2];
+			nations[index2] = _n;
 		}
-		
-		
+		System.out.println();
+		for(String n : nations) {
+			System.out.printf("%s\t",n);
+		}
 	}
 	
 }
---
+++ SpMVC_003/src/main/java/com/callor/contact/exec/ArrayV7.java
@@ -0,0 +1,86 @@
+package com.callor.contact.exec;
+
+public class ArrayV7 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * 정수형 배열 45개를 만들고
+		 * index 0 ~ 44 까지 요소에 1 ~ 45까지의 정수를 저장
+		 */
+		int[] nums = new int[45];
+		for(int i = 0 ; i < nums.length ; i++) {
+			nums[i] = i + 1;
+		}
+		for(int n : nums) {
+			System.out.printf("%d\t",n);
+		}
+		
+		System.out.printf("\n%d\n",nums[44]);
+
+		
+		/*
+		 * 배열 45개의 요소를 Shuffle 하고
+		 * 결과를 출력
+		 */
+		// for(int i = nums.length ; i > 0 ; i--) {}
+		for(int i = 0 ; i < nums.length ; i++) {
+			int index1 = (int)(Math.random() * nums.length);
+			int index2 = (int)(Math.random() * nums.length);
+			
+			// 숫자형 변수 2개의 값 교환하기
+//			System.out.printf("%d, %d\t", nums[index1], nums[index2]);
+//			nums[index1] = nums[index1] + nums[index2];
+//			System.out.printf("%d, %d\t", nums[index1], nums[index2]);
+//			nums[index2] = nums[index1] - nums[index2];
+//			System.out.printf("%d, %d\t", nums[index1], nums[index2]);
+//			nums[index1] = nums[index1] - nums[index2];
+//			System.out.printf("%d, %d \n", nums[index1], nums[index2]);
+			
+			int _n = nums[index1];
+			nums[index1] = nums[index2];
+			nums[index2] = _n;
+		}
+//		System.out.println();
+//		for(int n : nums) {
+//			System.out.printf("%d\t",n);
+//		}
+//		System.out.println();
+		
+		int[] lottos = new int[6];
+		// nums 배열의 0 부터 5까지 요소를
+		// lottos 0 부터 5까지 요소에 각각 복사
+		for(int i = 0 ; i < 6 ; i++) {
+			lottos[i] = nums[i];
+		}
+		
+		System.out.println();
+		for(int n : lottos) {
+			System.out.printf("%d\t",n);
+		}
+		
+		/*
+		 * lottos 배열에 저장된 각 값들을
+		 * 오름차순 정렬하여 출력하기
+		 */
+		for(int i = 0 ; i < lottos.length ; i++) {
+			
+			for(int j = i + 1 ; j < lottos.length; j++) {
+				if(lottos[i] > lottos[j]) {
+					int _n = lottos[i];
+					lottos[i] = lottos[j];
+					lottos[j] = _n;
+				}
+			}
+		}
+		System.out.println();
+		for(int n : lottos) {
+			System.out.printf("%d\t",n);
+		}
+		
+		
+		
+		
+		
+	}
+}
