a1aae8c9f8c10ad3ad9bcdf18458acdefdc864e3
callor <callor@callor.com>
2023-02-08 11:19:38 +0900

    naver

---
+++ SpMVC_007_MyBooks/src/main/java/com/callor/books/config/NaverSecretSample.java
@@ -0,0 +1,14 @@
+package com.callor.books.config;
+
+/**
+ * Naver 도서검색을 위하여
+ * 자신의 Application CLIENT_ID, CLIENT_SECRET 를 아래변수에
+ * 추가한 후 클래스이름을 NaverScret 로 변경한 후 프로젝트를 Run 하시오
+ * @author callor
+ *
+ */
+public class NaverSecretSample {
+	public final static String CLIENT_ID ="";
+	public final static String CLIENT_SECRET = "";
+	public final static String NAVER_BOOK_URL_JSON = "https://openapi.naver.com/v1/search/book.json";
+}
